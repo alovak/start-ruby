9606d9f685b83ac02849ed120c7860610cf03b9e
Pavel Gabriel <alovak@gmail.com>
2015-10-27 11:30:36 +0300

    add spec for metadata

--- spec/lib/start/charge_spec.rb
+++ spec/lib/start/charge_spec.rb
@@ -23,6 +23,30 @@ describe Start::Charge do
     expect(response['captured_amount']).to eq(400)
   end
 
+  context 'when metadata parameter is passed' do
+    it 'creates charge with metadata' do
+      response = Start::Charge.create(
+        :amount => 400,
+        :currency => "usd",
+        :email => "ahmed@example.com",
+        :card => {
+          :name => "Abdullah Ahmed",
+          :number => "4242424242424242",
+          :exp_month => 11,
+          :exp_year => 2016,
+          :cvc => 123
+        },
+        :description => "Charge for test@example.com",
+        :metadata => {
+          :reference_id => "12334567890",
+          :tag => "new"
+        }
+      )
+
+      expect(response['metadata']).to eq({"reference_id"=>"12334567890", "tag"=>"new"})
+    end
+  end
+
    it "must list created charges" do
     response = Start::Charge.all()
 
